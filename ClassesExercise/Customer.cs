690c3e20c94b81317b6d3bd44fc3d25f9f01fca2
RadicalPet <sarah@radicalpet.net>
2014-12-09 04:25:25 +0100

    initial commit, customer class, constructor calling constructor

---
+++ ClassesExercise/Customer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    class Customer
+    {
+        private static int numberOfCustomers = 0;
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Email { get; set; }
+        public DateTime SignupDate { get; set; }
+
+        private int customerId; 
+
+        public Customer(string name, DateTime dateOfBirth)
+            : this(name, dateOfBirth, "no email given", DateTime.Now)
+        {
+        }
+        public Customer(string name, DateTime dateOfBirth, string email)
+            : this(name, dateOfBirth, email, DateTime.Now)
+        {
+        }
+        public Customer(string name, DateTime dateOfBirth, DateTime signupDate)
+            : this(name, dateOfBirth, "no email given", signupDate)
+        {
+        }
+        
+        public Customer(string name, DateTime dateOfBirth, string email, DateTime signupDate)
+        {
+            this.Name = name;
+            this.DateOfBirth = dateOfBirth;
+            this.Email = email;
+            this.SignupDate = signupDate;
+            Customer.numberOfCustomers = Customer.numberOfCustomers + 1;
+            this.customerId = Customer.numberOfCustomers;
+        }
+        public static int getNumberOfCustomers()
+        {
+            return Customer.numberOfCustomers;
+        }
+  
+    }
+}
---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sarah";
+            string birthDate = "16/04/1985";
+            string email = "sahara.braun@gmail.com";
+            DateTime dateOfBirth = Convert.ToDateTime(birthDate);
+
+            string name2 = "Elise";
+            string birthDate2 = "01/02/1971";
+            DateTime dateOfBirth2 = Convert.ToDateTime(birthDate2); 
+
+            Customer customer1 = new Customer(name, dateOfBirth, email);
+            Customer customer2 = new Customer(name2, dateOfBirth2);
+
+            Console.WriteLine("Name: {0}\nBirth Date: {1}\nEmail: {2}\nSignup Date: {3}\n", customer1.Name, customer1.DateOfBirth, customer1.Email, customer1.SignupDate);
+            Console.WriteLine("Name: {0}\nBirth Date: {1}\nEmail: {2}\nSignup Date: {3}\n", customer2.Name, customer2.DateOfBirth, customer2.Email, customer2.SignupDate);
+            Console.WriteLine("Number of Customers: {0}", Customer.getNumberOfCustomers().ToString());
+        }
+    }
+}
